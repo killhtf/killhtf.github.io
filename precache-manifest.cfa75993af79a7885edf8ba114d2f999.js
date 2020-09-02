7d85b602ec383e0caeb6a0c6a0af1b488d4c3b77
killhtf <nemytyshew@yandex.ru>
2020-09-02 09:39:55 +0300

    Updates

---
+++ precache-manifest.cfa75993af79a7885edf8ba114d2f999.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b873cb47aa6c9e44e3946348e52f64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dc6754007e70a3f574e",
+    "url": "/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "27b0a3e83209f6ae8f59",
+    "url": "/static/css/main.ead71ef5.chunk.css"
+  },
+  {
+    "revision": "9dc6754007e70a3f574e",
+    "url": "/static/js/2.dc5f8689.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.dc5f8689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b0a3e83209f6ae8f59",
+    "url": "/static/js/main.2dd66e9f.chunk.js"
+  },
+  {
+    "revision": "8b0f391bfdd48764ca85",
+    "url": "/static/js/runtime-main.15e679b5.js"
+  },
+  {
+    "revision": "5f2ba1b1da6975467bf8f33220b3b0ac",
+    "url": "/static/media/ChocoCookie240.5f2ba1b1.png"
+  },
+  {
+    "revision": "cfee21d8af16c6da5fc3b1fb87e4272f",
+    "url": "/static/media/doublecookies av.cfee21d8.png"
+  }
+]);
