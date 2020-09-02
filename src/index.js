1be88e95c29353205d8cad0ce007939fd8b7d393
killhtf <nemytyshew@yandex.ru>
2020-09-02 09:36:16 +0300

    update styles and main page, translation

--- src/index.js
+++ src/index.js
@@ -4,38 +4,38 @@ import './index.css';
 import logo from "./images/doublecookies av.png";
 import chocolateCookie from './images/ChocoCookie240.png'
 import {GithubOutlined, TwitterOutlined} from '@ant-design/icons';
-import {Col, Image, Layout, Menu, Row} from 'antd';
+import {Col, Image, Layout, Row} from 'antd';
 import {useTranslation} from 'react-i18next';
 import './i18n';
 import FandomIcon from "./FandomIcon";
 
-const {Header, Content, Footer} = Layout;
+const {Content, Footer} = Layout;
 
 function MainPage() {
-    const { t } = useTranslation(['about', 'namespace2']);
+    const {t} = useTranslation(['about', 'namespace2']);
     return (
         <Layout className="layout">
-            <Header>
-                <div className="logo" />
-                <Menu
-                    theme="light"
-                    mode="horizontal"
-                    defaultSelectedKeys={['1']}
-                    style={{ lineHeight: '32px', height: '32px' }}
-                >
-                    <Menu.Item key="1">Home</Menu.Item>
-                    <Menu.Item key="3">
-                        <a href="https://github.com/DoubleCookies" target="_blank" rel="noopener noreferrer"><GithubOutlined />Github</a>
-                    </Menu.Item>
-                </Menu>
-            </Header>
+            {/*<Header>*/}
+            {/*    <div className="logo" />*/}
+            {/*    <Menu*/}
+            {/*        theme="light"*/}
+            {/*        mode="horizontal"*/}
+            {/*        defaultSelectedKeys={['1']}*/}
+            {/*        style={{ lineHeight: '32px', height: '32px' }}*/}
+            {/*    >*/}
+            {/*        <Menu.Item key="1">Home</Menu.Item>*/}
+            {/*        <Menu.Item key="3">*/}
+            {/*            <a href="https://github.com/DoubleCookies" target="_blank" rel="noopener noreferrer"><GithubOutlined />Github</a>*/}
+            {/*        </Menu.Item>*/}
+            {/*    </Menu>*/}
+            {/*</Header>*/}
             <Content>
                 <div className="content-title">
                     <Row type="flex" gutter={48} align="middle">
-                        <Col xs={{ span: 6 }} sm={{ span: 6 }}>
+                        <Col span={6} offset={2}>
                             <Image src={logo} alt={"Cookies"} className={"myImg"} />
                         </Col>
-                        <Col xs={{ span: 16, offset: 2 }} sm={{ span: 17, offset: 1 }}>
+                        <Col span={15} offset={1}>
                             <div>
                                 <span className="headings text-big">DoubleCookies</span>
                             </div>
@@ -43,8 +43,12 @@ function MainPage() {
                     </Row>
                 </div>
                 <div className="content-main">
-                    <div className="headings text-medium">{t("about:title")}</div>
-                    <div>{t("about:intro")}</div>
+                    <Row gutter={[16, 16]}>
+                        <Col span={24}><div className="headings text-medium">{t("about:title")}</div></Col>
+                    </Row>
+                    <Row gutter={[16, 16]}>
+                        <Col span={24}>{t("about:intro")}</Col>
+                    </Row>
                 </div>
             </Content>
             <Footer style={{paddingTop: 36, overflowY: 'hidden'}}>
@@ -54,7 +58,7 @@ function MainPage() {
                         <a href="https://twitter.com/BigDoubleCookie" target="_blank" rel="noopener noreferrer"><TwitterOutlined style={{fontSize: 32}} /></a>
                         <a href="https://community.fandom.com/wiki/User:DoubleCookies" target="_blank" rel="noopener noreferrer"><FandomIcon className="fandomIcon" style={{width: 32, height: 32}} /></a>
                     </div>
-                    <Image src={chocolateCookie} alt={"ChocoCookie"} className={"footerImg"} />
+                    <Image src={chocolateCookie} alt={"ChocolateCookie"} className={"footerImg"} />
                 </div>
             </Footer>
         </Layout>
